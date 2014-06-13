dd70bec64af70b71072da03c65eb671a1c3d2685
Jessica Stokes <hello@jessicastokes.net>
2014-06-13 18:53:02 +1000

    Simplify scripts

--- scripts.js
+++ scripts.js
@@ -1,56 +1,5 @@
 (function() {
 
-	/* === Textual Event Shim ===
-	 * Mock out Textual callbacks to be DOM events
-	 * This provides a slightly nicer API to work with */
-
-    function createEventEmitter(type) {
-
-        return function EventEmitter() {
-
-            var target = document.body,
-                event = new CustomEvent(type, {
-                    detail: Array.prototype.slice.call(arguments)
-                });
-
-            if (type === 'newMessagePostedToView' || type === 'viewPositionMovedToLine') {
-                var newTarget = document.getElementById('line-' + arguments[0]);
-                if (newTarget) {
-                    target = newTarget;
-                }
-            }
-
-            target.dispatchEvent(event);
-
-        }
-
-    }
-
-    [
-    	'viewInitiated',
-	    'newMessagePostedToView',
-	    'historyIndicatorAddedToView',
-	    'historyIndicatorRemovedFromView',
-	    'topicBarValueChanged',
-	    'viewContentsBeingCleared',
-	    'viewFinishedLoading',
-	    'viewFinishedReload',
-	    'viewFontSizeChanged',
-	    'viewPositionMovedToBottom',
-	    'viewPositionMovedToHistoryIndicator',
-	    'viewPositionMovedToLine',
-	    'viewPositionMovedToTop',
-	    'sidebarInversionPreferenceChanged',
-	    'handleEvent'
-    ].forEach(function(event) {
-
-        Object.defineProperty(Textual, event, {
-            enumerable: true,
-            value: createEventEmitter(event)
-        });
-
-    });
-
 	Textual.fadeInLoadingScreen = function() {
 
 		// Override loading screen fade
@@ -63,24 +12,15 @@
 
 	};
 
-	document.addEventListener('DOMContentLoaded', function() {
+	Textual.viewFinishedLoading = Textual.viewFinishedReload = function() {
 
-		console.log("Theme Script Loaded");
+		console.debug("View Finished Loading");
 
-		function onReady() {
+		Textual.scrollToBottomOfView();
+		Textual.fadeInLoadingScreen();
 
-			console.debug("View Finished Loading");
+	};
 
-			Textual.scrollToBottomOfView();
-			Textual.fadeInLoadingScreen();
-
-		}
-
-		document.body.addEventListener('viewInitiated', function(e) { console.log("view initiated", e); });
-
-		document.body.addEventListener('viewFinishedLoading', onReady);
-		document.body.addEventListener('viewFinishedReload', onReady);
-
-	});
+	console.log("Theme Script Loaded");
 
 }());
