157b8d27bef0a104f9e2b50b313c07a0b6b26b52
Jessica Stokes <hello@jessicastokes.net>
2014-06-19 22:17:21 +1000

    Convert to Sass, add Gulp build system, Nox theme

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+(function() {
+
+	'use strict';
+
+	var gulp = require('gulp'),
+		clean = require('gulp-clean'),
+		sass = require('gulp-ruby-sass'),
+		theme = require('./package');
+
+	function buildTheme(name) {
+
+		if (typeof name !== 'string') {
+			name = 'Classic';
+		}
+
+		[
+			gulp.src([
+				'./readme.md',
+				'./copyright.txt',
+				'./src/scripts.js'
+			]),
+			gulp.src('./src/scss/design.scss')
+				.pipe(sass({
+					loadPath: ['./src/themes/' + name + '/scss/']
+				}))
+		].forEach(function(item) {
+			item.pipe(gulp.dest('./build/' + theme.title + ' (' + name + ')/'));
+		});
+
+		gulp.src([
+			'./src/Data/**/*',
+			'./src/themes/' + name + '/Data/**/*'
+		]).pipe(gulp.dest('./build/' + theme.title + ' (' + name + ')/Data/'));
+
+	}
+
+	gulp.task('clean', function() {
+		gulp.src('./build/**/*', {read: false})
+			.pipe(clean())
+	});
+
+	gulp.task('classic', function() {
+		buildTheme('Classic');
+	});
+
+	gulp.task('nox', function() {
+		buildTheme('Nox');
+	});
+
+	gulp.task('default', function() {
+		gulp.start('classic', 'nox');
+	});
+
+}());
