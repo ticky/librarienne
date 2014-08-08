d23533dade2e85ab59d69c2c4d28fd223324454c
Jessica Stokes <hello@jessicastokes.net>
2014-08-08 18:30:59 +1000

    Convert build system to use directory structure
    - variants can now be added simply by creating the directory structure and overriding settings

--- gulpfile.js
+++ gulpfile.js
@@ -4,69 +4,80 @@
 
 	var gulp = require('gulp'),
 		clean = require('gulp-clean'),
+		glob = require('glob'),
 		minify = require('gulp-minify-html'),
+		path = require('path'),
 		sass = require('gulp-ruby-sass'),
 		zip = require('gulp-zip'),
 		theme = require('./package');
 
-	function buildTheme(name) {
+	gulp.task('default', function() {
+	
+		glob.sync('./src/variants/*')
+		.forEach(function(dir) {
 
-		if (typeof name !== 'string') {
-			name = 'Classic';
-		}
+			var variant = path.basename(dir);
 
-		[
+			[
+				gulp.src([
+					'./readme.md',
+					'./copyright.txt',
+					'./src/scripts.js'
+				]),
+				gulp.src('./src/scss/design.scss')
+				.pipe(
+					sass({
+						loadPath: ['./src/variants/' + variant + '/scss/'],
+						style: 'compressed'
+					})
+				)
+			]
+			.forEach(function(item) {
+				item.pipe(
+					gulp.dest('./build/' + theme.title + ' (' + variant + ')/')
+				);
+			});
+
 			gulp.src([
-				'./readme.md',
-				'./copyright.txt',
-				'./src/scripts.js'
-			]),
-			gulp.src('./src/scss/design.scss')
-				.pipe(sass({
-					loadPath: ['./src/themes/' + name + '/scss/'],
-					style: 'compressed'
-				}))
-		].forEach(function(item) {
-			item.pipe(gulp.dest('./build/' + theme.title + ' (' + name + ')/'));
+				'./src/Data/Settings/*',
+				'./src/variants/' + variant + '/Data/Settings/*'
+			])
+			.pipe(
+				gulp.dest('./build/' + theme.title + ' (' + variant + ')/Data/Settings/')
+			);
+
+			gulp.src([
+				'./src/Data/Templates/**/*.mustache',
+				'./src/variants/' + variant + '/Data/Templates/**/*.mustache'
+			])
+			.pipe(
+				minify({
+					quotes: true // disable removing quotes
+				})
+			)
+			.pipe(
+				gulp.dest('./build/' + theme.title + ' (' + variant + ')/Data/Templates/')
+			);
+
 		});
 
-		gulp.src([
-			'./src/Data/Settings/*',
-			'./src/themes/' + name + '/Data/Settings/*'
-		]).pipe(gulp.dest('./build/' + theme.title + ' (' + name + ')/Data/Settings/'));
-
-		gulp.src([
-			'./src/Data/Templates/**/*.mustache',
-			'./src/themes/' + name + '/Data/Templates/**/*.mustache'
-		])
-		.pipe(minify({
-			quotes: true // disable removing quotes
-		}))
-		.pipe(gulp.dest('./build/' + theme.title + ' (' + name + ')/Data/Templates/'));
-
-	}
+	});
 
 	gulp.task('clean', function() {
 		gulp.src('./build/**/*', {read: false})
-			.pipe(clean())
+		.pipe(clean())
 	});
 
-	gulp.task('classic', function() {
-		buildTheme('Classic');
-	});
+	gulp.task('zip', ['default'], function() {
 
-	gulp.task('nox', function() {
-		buildTheme('Nox');
-	});
-
-	gulp.task('default', function() {
-		gulp.start('classic', 'nox');
-	});
-
-	gulp.task('zip', ['classic', 'nox'], function() {
 		gulp.src('./build/**/*')
-			.pipe(zip(theme.title + ' ' + theme.version + '.zip'))
-			.pipe(gulp.dest('./dist/'))
+		.pipe(
+			zip(theme.title + ' ' + theme.version + '.zip')
+		)
+		.pipe(
+			gulp.dest('./dist/')
+		)
+
 	});
 
 }());
